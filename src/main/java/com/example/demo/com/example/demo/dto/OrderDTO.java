a8a24c154b6d560560498923a31714c12a60022a
Tiago Ventura <tfj.ventura@gmail.com>
2022-01-29 02:28:24 +0100

    fix total

--- src/main/java/com/example/demo/com/example/demo/dto/OrderDTO.java
+++ src/main/java/com/example/demo/com/example/demo/dto/OrderDTO.java
@@ -22,11 +22,29 @@ public class OrderDTO implements Serializable {
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
+	private Double total;
 
 	private List<ProductDTO> products = new ArrayList<>();
 
 	public OrderDTO() {
 	}
+	
+	
+
+	public OrderDTO(Long id, String address, Double latitude, 
+			Double longitude, Instant moment, OrderStatus status, Double total
+			) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
+		this.total = total;
+	}
+
+
 
 	public OrderDTO(Order entity) {
 		this.id = entity.getId();
@@ -35,6 +53,7 @@ public class OrderDTO implements Serializable {
 		this.longitude = entity.getLongitude();
 		this.moment = entity.getMoment();
 		this.status = entity.getStatus();
+		this.total = entity.getTotal();
 
 		this.products = entity.getProducts().stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
@@ -90,5 +109,17 @@ public class OrderDTO implements Serializable {
 	public List<ProductDTO> getProducts() {
 		return products;
 	}
+
+
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+	
+	
 
 }
